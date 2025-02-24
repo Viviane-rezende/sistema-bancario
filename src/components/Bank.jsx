36c7719a2a7d80ac1ebc98fa17a17ce677fddba2
Viviane Rezende <vivianerezende95@gmail.com>
2025-02-23 22:12:16 -0300

    Sistema-Bancario01

--- src/components/Bank.jsx
+++ src/components/Bank.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 function Bank() {
     const [conta, setConta] = useState({
@@ -11,43 +11,108 @@ function Bank() {
         saldo: 0
     })
 
-    const [Op, setOp] = useState("")
+    const cred = useRef()
+    const deb = useRef()
 
-    function Abrir(props) {
+    function Abrir() {
         setConta({
-            numConta: Math.random(),
+            numConta: Math.floor(Math.random() * 1000),
             nomeAg: "AgBanco",
-            numAg: Math.random(),
+            numAg: Math.floor(Math.random() * 1000),
             nomeBanco: "BancoBR",
             numBanco: 1,
             tipo: 1,
             saldo: 0
         })
-        setOp("Conta aberta com sucesso!")
+
+
+        alert("Conta aberta com sucesso!" + "\n\nNúmero da conta: " + conta.numConta + "\nNome Agência: " + conta.nomeAg + "\nNum Agência: " + conta.numAg + "\nNome banco: " + conta.nomeBanco + "\nNum Banco: " + conta.numBanco + "\nTipo: " + conta.tipo + "\nSaldo: " + conta.saldo)
     }
 
     function Encerrar() {
 
+        if (conta.saldo >= 0) {
+
+            setConta(prevState => ({
+                ...prevState,
+                tipo: 4,
+                saldo: 0,
+                prevSaldo: prevState.saldo
+            }))
+        }
+
+
+        alert("Conta encerrada com sucesso!" + "\n\nNúmero da conta: " + conta.numConta + "\nNome Agência: " + conta.nomeAg + "\nNum Agência: " + conta.numAg + "\nNome banco: " + conta.nomeBanco + "\nNum Banco: " + conta.numBanco + "\nTipo: " + conta.tipo + "\nSaldo: " + conta.saldo + "\nSaldo anterior: " + conta.prevSaldo)
     }
 
     function ConsultarSaldo() {
+        alert("Consultar saldo" + "\n\nSaldo da conta: " + conta.saldo)
 
     }
 
     function Creditar() {
+        if (conta.tipo != 4) {
+
+            const credito = cred.current.value
+            let totalSaldo = parseFloat(credito) + parseFloat(conta.saldo)
+
+            setConta(prevState => ({
+                ...prevState,
+                saldo: totalSaldo
+            }))
+
+            alert("Você creditou: " + credito + "\nSaldo atual: " + totalSaldo)
+        }
 
     }
 
     function Debitar() {
+        if (conta.tipo != 4) {
+            const debit = deb.current.value
+            let totalSaldo = parseFloat(conta.saldo) - parseFloat(debit)
+
+
+            setConta(prevState => ({
+                ...prevState,
+                saldo: totalSaldo
+            }))
+
+            alert("Você debitou: " + debit + "\nSaldo atual: " + totalSaldo)
+        }
 
     }
-    return (<div>
-                <h3>Bem vindo ao banco!</h3>
-            </div>
-    <div>
-        <h2>Abrir conta bancária</h2>
-        <button>Abror Conta</button>
-    </div>
+    return (<>
+        <div>
+            <h1>Bem vindo ao banco!</h1>
+        </div>
+        <div>
+            <h2>Abrir conta bancária</h2>
+            <button onClick={Abrir}>Abrir Conta</button>
+
+        </div>
+        <div>
+            <h2>Encerrar conta bancária</h2>
+            <button onClick={Encerrar}>Encerrar Conta</button>
+
+        </div>
+        <div>
+            <h2>Consultar saldo atual </h2>
+            <button onClick={ConsultarSaldo}>Consultar Saldo</button>
+
+        </div>
+        <div>
+            <h2>Creditar na conta</h2>
+            <input type='number' ref={cred}></input><br /><br />
+            <button onClick={Creditar}>Creditar</button>
+
+        </div>
+        <div>
+            <h2>Debitar da conta</h2>
+            <input type='number' ref={deb}></input><br /><br />
+            <button onClick={Debitar}>Debitar</button>
+
+        </div>
+    </>
     )
 }
 
